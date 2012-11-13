7404106ff87611c7e3bd21d1b3b7da52cda90b1c
Shuyang <linshuy@gmail.com>
2012-11-12 19:53:08 -0600

    add script for collecting papers

---
+++ script/download_papers.py
@@ -0,0 +1,267 @@
+import urllib2
+import time
+import random
+for i in range(1001,1161):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/P/P10/P10-'+ str(i)+ '.pdf')
+	localFile = open('2010/P10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('P10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(3001,3020):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/P/P10/P10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_Student_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_student_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+
+for i in range(2901,2928):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W10/W10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(3001,3024):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W10/W10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(1701,1764):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W10/W10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+
+for i in range(1801,1845):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W10/W10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(1901,1922):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W10/W10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(2101,2112):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W10/W10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(2601,2609):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W10/W10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(2301,2318):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W10/W10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+
+for i in range(2001,2011):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W10/W10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(2401,2421):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W10/W10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(2201,2212):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W10/W10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+
+for i in range(2501,2507):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W10/W10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(2701,2709):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W10/W10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(2801,2810):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W10/W10-'+ str(i)+ '.pdf')
+	localFile = open('2010/W10_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W10_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+
+for i in range(1001,1100):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/P/P07/P07-'+ str(i)+ '.pdf')
+	localFile = open('2007/P07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('P07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(3001,3017):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/P/P07/P07-'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_Student_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_student_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(2201,2221):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W07/W07-'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(701,739):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W07/W07-0'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(801,815):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W07/W07-0'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(901,913):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W07/W07-0'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(1001,1034):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W07/W07-'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(1101,1111):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W07/W07-'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(1201,1221):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W07/W07-'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(1302,1318):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W07/W07-'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(1401,1432):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W07/W07-'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(1501,1531):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W07/W07-'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(1601,1609):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W07/W07-'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(1701,1714):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W07/W07-'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(1801,1808):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W07/W07-'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(601,613):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W07/W07-0'+ str(i)+ '.pdf')
+	localFile = open('2007/W07_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('W07_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+
+
+
