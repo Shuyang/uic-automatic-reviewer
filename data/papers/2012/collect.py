8e426328119803f1a554d6fa52a2c2169e565136
Shuyang <linshuy@gmail.com>
2012-11-12 19:52:09 -0600

    add papers of ACL2012,2010,2007

---
+++ data/papers/2012/collect.py
@@ -0,0 +1,81 @@
+import urllib2
+import time
+import random
+for i in range(3601,3627):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W12/W12-'+ str(i)+ '.pdf')
+	localFile = open('W12_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('fullpaper_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+import os
+for i in range(1001,1112):
+	os.rename('P_'+ str(i) + '.pdf','P11_'+ str(i) + '.pdf')
+
+
+for i in range(3701,3718):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W12/W12-'+ str(i)+ '.pdf')
+	localFile = open('W12_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('fullpaper_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+
+for i in range(3801,3811):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W12/W12-'+ str(i)+ '.pdf')
+	localFile = open('W12_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('fullpaper_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(3901,3905):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W12/W12-'+ str(i)+ '.pdf')
+	localFile = open('W12_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('fullpaper_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(4001,4007):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W12/W12-'+ str(i)+ '.pdf')
+	localFile = open('W12_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('fullpaper_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(4101,4109):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W12/W12-'+ str(i)+ '.pdf')
+	localFile = open('W12_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('fullpaper_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(4201,4214):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W12/W12-'+ str(i)+ '.pdf')
+	localFile = open('W12_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('fullpaper_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+for i in range(4301,4307):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W12/W12-'+ str(i)+ '.pdf')
+	localFile = open('W12_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('fullpaper_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
+
+
+for i in range(4401,4413):
+	u = urllib2.urlopen('http://aclweb.org/anthology-new/W/W12/W12-'+ str(i)+ '.pdf')
+	localFile = open('W12_'+ str(i) + '.pdf','wb')
+	localFile.write(u.read())
+	localFile.close()
+	print('fullpaper_'+ str(i) + '.pdf')
+	time.sleep(random.randrange(5000)/1000+0.5)
